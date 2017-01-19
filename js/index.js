a26cdcac287ead21feba6478136a247f78a8847c
David Caldwell <ddcaldwell87@gmail.com>
2017-01-18 22:47:17 -0500

    added scripts for user inputed city (broken)

--- js/index.js
+++ js/index.js
@@ -1,69 +1,99 @@
-$(document).ready(function(){
+$(document).ready(function()
+{
   
   var lon;
   var lat;
-  var userInput;
+  var local = true;
+  var nav = navigator.geolocation;
 
-  if (navigator.geolocation) 
+ 
+  // Gets the browsers location
+  navigator.geolocation.getCurrentPosition(function(position)
   {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      // Gets browsers location
-      
-      lon = position.coords.longitude;
-      lat = position.coords.latitude;
-
-      
-  // API from Open Weather API
-  var api = "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid=12305dcb1bc441c342264e813a38d17d";
-
-  var userCity = "http://api.openweathermap.org/data/2.5/weather?q=richmond,in&appid=12305dcb1bc441c342264e813a38d17d"
-  
-  $.getJSON(api, function(data)
-  {
-    // Location based JSON call from Open Weather API
-      
-    var weatherType = data.weather[0].description;
-    var kTemp = data.main.temp;
-    var city = data.name;
-    var windSpeed = data.wind.speed;
     
-    // Kelvin to farhrenheit conversion
-    var fTemp = Math.ceil((kTemp) * (9 / 5) - 459.67);
-    // Kelvin to celsius conversion
-    var cTemp = Math.ceil(kTemp - 273);
-    var temp = $("#temp");
+    lon = position.coords.longitude;
+    lat = position.coords.latitude;
 
-    document.getElementById("fSwap").onclick = function(temp)
-    {
-      $("#temp").html(fTemp + "&deg;F");
-    };
+    // Coordinate API from Open Weather API
+    var api = "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid=12305dcb1bc441c342264e813a38d17d";
 
-    document.getElementById("cSwap").onclick = function(temp)
+    $.getJSON(api, function(data)
     {
-      $("#temp").html(cTemp + "&deg;C");
-    };
+      // Coordinate based JSON call from Open Weather API
 
-    function weatherTypeCase(str)
-    {
-      str = str.toLowerCase().split(' ');
-      for (var i = 0; i < str.length; i++)
+      function weatherData()
       {
-        str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
-      }
-      return str.join(' ');
-    }
+        var weatherType = data.weather[0].description;
+        var kTemp = data.main.temp;
+        var city = data.name;
+        var windSpeed = data.wind.speed;
 
-    console.log(city);
-    console.log(weatherType);
-    console.log(fTemp);
-    console.log(lon);
-    console.log(lat);
-    console.log(api);
-    console.log(temp);
-    $("#city").html(city);
-    $("#weatherType").html(weatherTypeCase(weatherType));
-    $("#temp").html(fTemp + "&deg;F");
+        // Kelvin to farhrenheit conversion
+        var fTemp = Math.ceil((kTemp) * (9 / 5) - 459.67);
+        // Kelvin to celsius conversion
+        var cTemp = Math.ceil(kTemp - 273);
+        var temp = $("#temp");
+
+        // Changes temp reading to either farhrenheit or celsius
+        $("#fSwap").click(function(temp)
+        {
+          $("#temp").html(fTemp + "&deg;F");
+        });
+
+        $("#cSwap").click(function(temp)
+        {
+          $("#temp").html(cTemp + "&deg;C");
+        });
+
+        // Title Cases the var weatherType weather description
+        function weatherTypeCase(str)
+        {
+          str = str.toLowerCase().split(' ');
+          for (var i = 0; i < str.length; i++)
+          {
+            str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+          }
+          return str.join(' ');
+        }
+
+        console.log(city);
+        console.log(weatherType);
+        console.log(fTemp);
+        console.log(lon);
+        console.log(lat);
+        console.log(api);
+        console.log(temp);
+        // Displays weather data in browser
+        $("#city").html(city);
+        $("#weatherType").html(weatherTypeCase(weatherType));
+        $("#temp").html(fTemp + "&deg;F");
+      }
+      weatherData();
+    });
   });
+
+  
+
+  var userCityInput;
+  var userStateInput;
+  var cityInput = $("#cityInput");
+  var stateInput = $("#stateInput");
+
+  // Displays user inputed city weather data (not working)
+  $("#cityInputButton").click(function()
+  {
+    
+    userCityInput = cityInput.val();
+    userStateInput = stateInput.val();
+
+    // City API from Open Weather API
+    var userCity = "http://api.openweathermap.org/data/2.5/weather?q="+userCityInput+","+userStateInput+"&appid=12305dcb1bc441c342264e813a38d17d"
+  
+    $.getJSON(userCity, function(data)
+    {
+      // City based JSON call from Open Weather API
+      weatherData();
+    });
   });
-  }
+
 });
